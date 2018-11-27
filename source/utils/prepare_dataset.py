fa0ad5a7d3941038830c08ec426d115e4ac9c0b1
Tyshchenko Andrey <ggoldenfeniks@gmail.com>
2018-11-27 15:28:39 +1000

    Don't generate existing masks

--- source/utils/prepare_dataset.py
+++ source/utils/prepare_dataset.py
@@ -14,18 +14,19 @@ from utils import cut_string
 import matplotlib.pyplot as plt
 
 
-def worker(n, files, result, classes, colors):
+def worker(n, files, result, classes, colors, regenerate):
 	pbar = tqdm(files, position=n)
 	for file in pbar:
 		pbar.set_description(cut_string(file))
 		dir, name = os.path.dirname(file), os.path.splitext(os.path.basename(file))[0]
+		bpath = os.path.join(dir, name + '.npz')
 		mpath = os.path.join(dir, name + '.mask.png')
-		mask = skimage.io.imread(mpath)
-		mask = np.apply_along_axis(lambda x: x == colors, 2, mask).sum(axis=3) == 3
-		ind = [i for i in range(len(classes)) if mask[::, ::, i].sum() > 0]
-		bpath = os.path.join(dir, name)
-		np.savez_compressed(bpath, mask=mask[::, ::, ind], classes=classes[ind])
-		result[n].append({ 'image': file, 'mask': mpath, 'boolmask': bpath + '.npz'})
+		if not os.path.exists(bpath) or regenerate:
+			mask = skimage.io.imread(mpath)
+			mask = np.apply_along_axis(lambda x: x == colors, 2, mask).sum(axis=3) == 3
+			ind = [i for i in range(len(classes)) if mask[::, ::, i].sum() > 0]
+			np.savez_compressed(bpath, mask=mask[::, ::, ind], classes=classes[ind])
+		result[n].append({ 'image': file, 'mask': mpath, 'boolmask': bpath})
 
 
 parser = argparse.ArgumentParser(description='Prepare dataset')
@@ -33,6 +34,7 @@ parser.add_argument('directory', metavar='DIR', help='Path to dataset', type=str
 parser.add_argument('--train', metavar='P', help='Train weight', type=float, default=0.8)
 parser.add_argument('--classes', metavar='C', help='Path to classes json', default='types.json')
 parser.add_argument('-t', '--threads', type=int, default=1, help='Number of threads', metavar='N')
+parser.add_argument('--regenerate', action='store_true', help='Regenerate mask even if it already exists')
 args = parser.parse_args()
 
 assert args.train <= 1, 'Train weight must be in range [0, 1]'
@@ -45,7 +47,7 @@ random.shuffle(files)
 result, threads, tn = [[] for i in range(args.threads)], [], ceil(len(files) / args.threads)
 
 for i in range(args.threads):
-	threads.append(threading.Thread(target=worker, args=(i, files[i * tn:(i + 1) * tn], result, classes, colors)))
+	threads.append(threading.Thread(target=worker, args=(i, files[i * tn:(i + 1) * tn], result, classes, colors, args.regenerate)))
 	threads[-1].start()
 
 for i in threads:
