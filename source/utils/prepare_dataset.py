c41c30a29843c252d5b7101de72eea70a8e5e89d
Tyshchenko Andrey <ggoldenfeniks@gmail.com>
2018-12-01 18:51:32 +1000

    Add process guard for Windows compatibility

--- source/utils/prepare_dataset.py
+++ source/utils/prepare_dataset.py
@@ -71,62 +71,64 @@ def writer(file, filters, queue, lock):
 			return
 
 
-parser = argparse.ArgumentParser(description='Prepare dataset')
-parser.add_argument('directory', metavar='DIR', help='Path to dataset', type=str)
-parser.add_argument('-f', '--file', type=str, default='dataset.hdf5', help='HDF5 dataset file', metavar='F')
-parser.add_argument('--train', metavar='P', help='Train weight', type=float, default=0.8)
-parser.add_argument('--classes', metavar='C', help='Path to classes json', default='types.json')
-parser.add_argument('-p', '--processes', type=int, default=1, help='Number of processes', metavar='N')
-parser.add_argument('--complevel', type=int, default=9, help='Compression level', metavar='L')
-parser.add_argument('--complib', type=str, default='blosc:lz4hc', help='Compression library', metavar='L')
-parser.add_argument('--save_path', action='store_true', help='Save image path')
-args = parser.parse_args()
+if __name__ == '__main__':
+	
+	parser = argparse.ArgumentParser(description='Prepare dataset')
+	parser.add_argument('directory', metavar='DIR', help='Path to dataset', type=str)
+	parser.add_argument('-f', '--file', type=str, default='dataset.hdf5', help='HDF5 dataset file', metavar='F')
+	parser.add_argument('--train', metavar='P', help='Train weight', type=float, default=0.8)
+	parser.add_argument('--classes', metavar='C', help='Path to classes json', default='types.json')
+	parser.add_argument('-p', '--processes', type=int, default=1, help='Number of processes', metavar='N')
+	parser.add_argument('--complevel', type=int, default=9, help='Compression level', metavar='L')
+	parser.add_argument('--complib', type=str, default='blosc:lz4hc', help='Compression library', metavar='L')
+	parser.add_argument('--save_path', action='store_true', help='Save image path')
+	args = parser.parse_args()
 
-assert args.train <= 1, 'Train weight must be in range [0, 1]'
+	assert args.train <= 1, 'Train weight must be in range [0, 1]'
 
-classes = np.array([i.encode('ascii') for i in json.load(open(os.path.join(args.directory, args.classes)))])
-colors = np.array([i for i in range(1, len(classes) + 1)], dtype='uint8')
+	classes = np.array([i.encode('ascii') for i in json.load(open(os.path.join(args.directory, args.classes)))])
+	colors = np.array([i for i in range(1, len(classes) + 1)], dtype='uint8')
 
-filters = tables.Filters(complevel=args.complevel, complib=args.complib)
-if os.path.exists(args.file):
-	file = tables.open_file(args.file, mode='r+')
-else:
-	file = tables.open_file(args.file, mode='w')
-	for i in ('image', 'mask', 'class_id'):
-		file.create_group(file.root, i)
-	file.create_carray(file.root, 'count', atom=tables.IntAtom(), shape=(1,), filters=filters)
-	file.create_earray(file.root, 'classes', atom=tables.StringAtom(25), shape=(0,), filters=filters)
-	file.create_earray(file.root, 'train', atom=tables.IntAtom(), shape=(0,), filters=filters)
-	file.create_earray(file.root, 'test', atom=tables.IntAtom(), shape=(0,), filters=filters)
-	file.root.count[0] = 0
+	filters = tables.Filters(complevel=args.complevel, complib=args.complib)
+	if os.path.exists(args.file):
+		file = tables.open_file(args.file, mode='r+')
+	else:
+		file = tables.open_file(args.file, mode='w')
+		for i in ('image', 'mask', 'class_id'):
+			file.create_group(file.root, i)
+		file.create_carray(file.root, 'count', atom=tables.IntAtom(), shape=(1,), filters=filters)
+		file.create_earray(file.root, 'classes', atom=tables.StringAtom(25), shape=(0,), filters=filters)
+		file.create_earray(file.root, 'train', atom=tables.IntAtom(), shape=(0,), filters=filters)
+		file.create_earray(file.root, 'test', atom=tables.IntAtom(), shape=(0,), filters=filters)
+		file.root.count[0] = 0
 
-unique(file.root.classes, classes)
+	unique(file.root.classes, classes)
 
-files = list(filter(lambda x: not x.endswith('.mask.png'), glob.iglob(os.path.join(args.directory, '**/*.png'), recursive=True)))
-count = len(files)
-random.shuffle(files)
+	files = list(filter(lambda x: not x.endswith('.mask.png'), glob.iglob(os.path.join(args.directory, '**/*.png'), recursive=True)))
+	count = len(files)
+	random.shuffle(files)
 
-pn, queue, lock, processes = ceil(count / args.processes), multiprocessing.Queue(), multiprocessing.Lock(), []
+	pn, queue, lock, processes = ceil(count / args.processes), multiprocessing.Queue(), multiprocessing.Lock(), []
 
-for i in range(args.processes):
-	processes.append(start_process(worker, (i, files[i * pn:(i + 1) * pn], classes, file.root.classes[:], colors, i * pn, lock, queue)))
+	for i in range(args.processes):
+		processes.append(start_process(worker, (i, files[i * pn:(i + 1) * pn], classes, file.root.classes[:], colors, i * pn, lock, queue)))
 
-writer(file, filters, queue, lock)
+	writer(file, filters, queue, lock)
 
-for i in processes:
-	print()
+	for i in processes:
+		print()
 
-if args.save_path:
-	if not '/path' in file.root:
-		file.create_earray(file.root, 'path', atom=tables.StringAtom(50), shape=(0,), filters=filters)
-	diff = file.root.count[0] - file.root.path.shape[0]
-	if diff:
-		file.root.path.append(np.full((diff,), ''))
-	file.root.path.append(np.fromiter((i[-50:] for i in files), dtype='|S50'))
+	if args.save_path:
+		if not '/path' in file.root:
+			file.create_earray(file.root, 'path', atom=tables.StringAtom(50), shape=(0,), filters=filters)
+		diff = file.root.count[0] - file.root.path.shape[0]
+		if diff:
+			file.root.path.append(np.full((diff,), ''))
+		file.root.path.append(np.fromiter((i[-50:] for i in files), dtype='|S50'))
 
-result = np.array(range(count)) + file.root.count[0]
-file.root.count[0] += count
+	result = np.array(range(count)) + file.root.count[0]
+	file.root.count[0] += count
 
-k = int(count * args.train)
-file.root.train.append(result[:k:])
-file.root.test.append(result[k::])
+	k = int(count * args.train)
+	file.root.train.append(result[:k:])
+	file.root.test.append(result[k::])
